c2efd307c05c621c150a01451b73bbdc15bdcc21
lucas <lucasaita4000@gmail.com>
2025-06-15 04:15:45 -0300

    build: conecta o Banco de Dados H2 na minha aplicacao Spring

--- src/main/java/dev/aluno/java10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/aluno/java10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,4 +1,6 @@
 package dev.aluno.java10x.CadastroDeNinjas.Ninjas;
 
-public interface NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }
