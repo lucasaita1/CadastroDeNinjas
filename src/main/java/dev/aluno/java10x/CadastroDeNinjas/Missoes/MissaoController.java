94ca703758bfc9905ce238d223068bc7f4a0a565
lucas <lucasaita4000@gmail.com>
2025-06-17 04:53:33 -0300

    chore: adiciona mocks temporários para testes de controllers

--- src/main/java/dev/aluno/java10x/CadastroDeNinjas/Missoes/MissaoController.java
+++ src/main/java/dev/aluno/java10x/CadastroDeNinjas/Missoes/MissaoController.java
@@ -1,4 +1,39 @@
 package dev.aluno.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
 public class MissaoController {
+
+    //Adicionar Ninja (CREATE)
+    @PostMapping("/criarmissao")
+    public String criarNinja(){
+        return "Missao Criada";
+    }
+
+    //MOSTRAR Ninja por ID (READ)
+    @GetMapping("/missoesID")
+    public String mostrarNinjasPorId(){
+        return "missoesID";
+    }
+
+    //VER TODOS NINJAS (READ)
+    @GetMapping("/missoes")
+    public String mostrarTodosNinjas(){
+        return "Todas missoes";
+    }
+
+    //Alterar dados dos ninjas (UPTADE)
+    @PutMapping("/modificarID")
+    public String alterarDadosDoNinja(){
+        return "Alterar missoes por ID";
+    }
+
+    //Deletar Ninjas (DELETE)
+    @DeleteMapping("/deletarmissoesID")
+    public String deletarNinjaPorId(){
+        return "Deletar missoes por Id";
+    }
+
 }
--- src/main/java/dev/aluno/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/aluno/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.aluno.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,35 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
      return ("Essa é a minha primeira mensagem e rota");
+    }
+
+    //Adicionar Ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja Criado";
+    }
+
+    //MOSTRAR Ninja por ID (READ)
+    @GetMapping("/todosID")
+    public String mostrarNinjasPorId(){
+        return "NinjaID";
+    }
+
+    //VER TODOS NINJAS (READ)
+   @GetMapping("/todos")
+    public String mostrarTodosNinjas(){
+        return "Todos Ninjas";
+    }
+
+    //Alterar dados dos ninjas (UPTADE)
+    @PutMapping("/alterarID")
+    public String alterarDadosDoNinja(){
+        return "Alterar ninja por ID";
+    }
+
+    //Deletar Ninjas (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId(){
+        return "Deletar ninja por Id";
     }
 }
