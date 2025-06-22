dc0fd0b8bcecd245ac409ca744f11581fb571e29
lucas <lucasaita4000@gmail.com>
2025-06-22 06:13:42 -0300

    feat: Adicionando a lógica de negócios na calasse MissaoService tornando-a funcional e capaz de listar missoes, criar missoes e deletar missoes por Id.

--- src/main/java/dev/aluno/java10x/CadastroDeNinjas/Missoes/MissaoService.java
+++ src/main/java/dev/aluno/java10x/CadastroDeNinjas/Missoes/MissaoService.java
@@ -1,4 +1,33 @@
 package dev.aluno.java10x.CadastroDeNinjas.Missoes;
 
+import dev.aluno.java10x.CadastroDeNinjas.Ninjas.NinjaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MissaoService {
+
+    private MissaoRepository missaoRepository;
+
+    public MissaoService(MissaoRepository missaoRepository) {
+        this.missaoRepository = missaoRepository;
+    }
+
+    //listar missoes
+    public List<MissaoModel> listarMissoes (){
+        return missaoRepository.findAll();
+    }
+
+    //criar missoes
+     public MissaoModel criarMissoes(MissaoModel missao){
+        return missaoRepository.save(missao);
+     }
+
+     //editar missoes
+
+    //deletar missoes
+    public void deletarMissoes (Long id) {
+        missaoRepository.deleteById(id);
+    }
 }
